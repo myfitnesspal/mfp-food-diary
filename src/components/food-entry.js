2ee26eed2c1f250d9abf6c958905fdfcfb0bf6fb
Emily.Guttenberg@myfitnesspal.com <emily.guttenberg@myfitnesspal.com>
2021-03-16 12:52:41 -0700

    Create food diary
    Food diary base
    
    set up some inputs
    
    most functionality complete
    
    start styling
    
    change function name
    
    add css
    
    clean up unnecessary files and add mroe css
    
    clean up
    
    update readme
    
    introduce errors
    
    introduce proptype error
    
    get rid of unnecessary div

---
+++ src/components/food-entry.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styles from './food-entry.module.css'
+import PropTypes from 'prop-types'
+
+function FoodEntry({name, description, descriptionVisible, onDelete}) {
+  return (
+    name ? 
+    <React.Fragment>
+      <div className={styles.foodEntryWrapper}>
+        <button className={styles.foodName}>{name}</button>
+        <button className={styles.deleteButton} onClick={onDelete}>-</button>
+      </div>   
+      <div className={styles.foodDescription}>
+        {descriptionVisible ? <p>Description: {description}</p> : null}
+      </div>
+    </React.Fragment>
+    : null
+  )
+}
+
+FoodEntry.propTypes = {
+  name: PropTypes.string.isRequired
+}
+
+export default FoodEntry
